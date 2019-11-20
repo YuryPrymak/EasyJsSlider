634ef7bd7aae3fb2293e3180f3be1b79f5353ab0
Yury Prymak <YuryPrymak@gmail.com>
2019-11-20 19:09:16 +0200

    Add new animations, fix bugs

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import Slider from './js/slider';
 
 import './scss/slider.scss';
-import './scss/animations/horizontal-shift.scss';
+import './scss/animations/fade-shift.scss';
 
 const slider = new Slider({
   slider: '.slider',
-  animationName: 'horizontal-shift',
-  animationDuration: 0.7,
+  animationName: 'fade-shift',
+  animationDuration: 1,
   useDots: true,
   useAutoPlay: true,
   autoPlayInterval: 5,
